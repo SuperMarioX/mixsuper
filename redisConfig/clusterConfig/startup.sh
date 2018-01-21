dc2d27f419f489c5b15eaa75a8a590eaa5c05c38
supermario <supermario_x@126.com>
2018-01-21 16:33:32 +0800

    add redis cluster config

---
+++ redisConfig/clusterConfig/startup.sh
@@ -0,0 +1,6 @@
+redis-server 6379/6379.conf
+redis-server 6380/6380.conf
+redis-server 6381/6381.conf
+redis-server 6382/6382.conf
+redis-server 6383/6383.conf
+redis-server 6384/6384.conf
---
+++ redisConfig/sentinelConfig/startup.sh
@@ -0,0 +1,5 @@
+redis-server ./redis_master_6379.conf
+redis-server ./redis_slave_6380.conf
+redis-server ./redis_slave_6381.conf
+redis-sentinel ./sentinel_63791.conf
+redis-sentinel ./sentinel_63792.conf
